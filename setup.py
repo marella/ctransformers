a44a8c04a6c4df25896dd1fefea60cdcd49eb689
Ravindra Marella <mv.ravindra007@gmail.com>
2023-06-01 00:33:26 +0530

    0.2.4

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ name = "ctransformers"
 
 setup(
     name=name,
-    version="0.2.3",
+    version="0.2.4",
     description="Python bindings for the Transformer models implemented in C/C++ using GGML library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
